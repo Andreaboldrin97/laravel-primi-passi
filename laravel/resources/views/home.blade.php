5b34b31f6626e893b52977fc2eb941033c80758c
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-06 15:36:13 +0200

    link created

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HELLO WORD LARAVEL</title>
 </head>
 
 <body>
-    <h1>HELLO WORD!!!!!</h1>
+    <a href="/m">
+        <h1>HELLO WORD!!!!!</h1>
+    </a>
 </body>
 
 </html>
